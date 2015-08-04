78049c318316ebbc3c74ef896f08ce52620a115a
Kasia Wilson <wilson.kasia@gmail.com>
2015-08-04 16:49:36 -0700

    Create plot2.r

---
+++ plot2.r
@@ -0,0 +1,12 @@
+td <- tempdir()
+tf <- tempfile(tmpdir=td,fileext=".zip")
+fileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+download.file(fileUrl, tf)
+dateDownloaded <- date()
+powerdata<-unzip(tf)
+powerdata2<-read.table(powerdata,skip=66638,nrows=2879,sep=";",na.strings="?",col.names=c("date","time","global_active_power","global_reactive_power","voltage","global_intensity","sub_metering_1","sub_metering_2","sub_metering_3"))
+powerdata2$date<-as.Date(powerdata2$date,format="%d/%m/%Y")
+powerdata2$datetime<-as.POSIXct(paste(powerdata2$date,powerdata2$time),format="%Y-%m-%d %H:%M:%S")
+plot2<-plot(powerdata2$datetime,powerdata2$global_active_power,xlab="",ylab="Global Active Power (kilowatts)",type="l")
+dev.copy(png,file="plot2.png",width = 480, height = 480, units = "px")
+dev.off()
